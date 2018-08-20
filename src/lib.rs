5ff5238a7296f5f3a7989766de7ebf6d582406f6
JolliestJames <martinja@oregonstate.edu>
2018-08-19 22:14:26 -0700

    Implement raindrops

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
 pub fn raindrops(n: usize) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    if (n%3 == 0) && (n%5 == 0) && (n%7 == 0 ) {
+        "PlingPlangPlong".to_string()
+    } else if (n%3 == 0) && (n%5 == 0) {
+        "PlingPlang".to_string()
+    } else if (n%5 == 0) && (n%7 == 0) {
+        "PlangPlong".to_string()
+    } else if (n%3 == 0) && (n%7 == 0) {
+        "PlingPlong".to_string()
+    } else if n%3 == 0 {
+        "Pling".to_string()
+    } else if n%5 == 0 {
+        "Plang".to_string()
+    } else if n%7 == 0 {
+        "Plong".to_string()
+    } else {
+        n.to_string()
+    }
 }
