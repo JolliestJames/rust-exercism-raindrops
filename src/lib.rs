db5c62169ea2aeacfce5c6e0e99462c9e44f57a3
JolliestJames <martinja@oregonstate.edu>
2018-08-25 12:01:14 -0700

    Refactor raindrops implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,16 @@
 pub fn raindrops(n: usize) -> String {
-    if (n%3 == 0) && (n%5 == 0) && (n%7 == 0 ) {
-        "PlingPlangPlong".to_string()
-    } else if (n%3 == 0) && (n%5 == 0) {
-        "PlingPlang".to_string()
-    } else if (n%5 == 0) && (n%7 == 0) {
-        "PlangPlong".to_string()
-    } else if (n%3 == 0) && (n%7 == 0) {
-        "PlingPlong".to_string()
-    } else if n%3 == 0 {
-        "Pling".to_string()
-    } else if n%5 == 0 {
-        "Plang".to_string()
-    } else if n%7 == 0 {
-        "Plong".to_string()
-    } else {
-        n.to_string()
+    let mut result = String::new();
+
+    match (n % 3, n%5, n%7) {
+        (0, 0, 0) => result.push_str("PlingPlangPlong"),
+        (0, 0, _) => result.push_str("PlingPlang"),
+        (0, _, 0) => result.push_str("PlingPlong"),
+        (_, 0, 0) => result.push_str("PlangPlong"),
+        (0, _, _) => result.push_str("Pling"),
+        (_, 0, _) => result.push_str("Plang"),
+        (_, _, 0) => result.push_str("Plong"),
+        (_, _, _) => result = n.to_string(),
     }
+
+    result
 }
